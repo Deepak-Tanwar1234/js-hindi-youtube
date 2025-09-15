77cbce98c162a4a4343e7495607cdb5a4a24b670
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-15 08:45:47 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,25 @@
 const accountId=1
 let accountName ="Deepak"
-let accountEmail="tan12@gmail.com"
+var accountEmail="tan12@gmail.com"
 var accountPassword="2154"
 accountcity="Delhi"
+let accountState
 
-//accountId=2
+//accountId=2   not allowed
 console.log(accountId)
 console.log(accountName)
+
+ accountName ="john"
+ accountEmail="john2@gmail.com"
+ accountPassword="214"
+accountcity="Newyork"
+
+console.log(accountName)
+console.log(accountEmail)
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountcity,accountState])
+
+/*
+Prefer not to use var
+because of issue in functional scope and block scope
+*/
