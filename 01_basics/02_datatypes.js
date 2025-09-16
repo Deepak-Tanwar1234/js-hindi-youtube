084f8bebc258214b7240c125f2a977aed013b3ae
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-16 11:09:02 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -8,3 +8,24 @@ console.log(3
     +3
 )
 // code readability should be high
+
+let name="Deepak"
+let age=25
+let young=true
+let state=""
+
+//Nummer=>2 to power 53
+//bigint
+//sting=""
+//boolean=>true or false
+//null=> standalone value
+//symbol=> for  uniqueness in react component
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(young))
+console.log(typeof(state))
+console.log(typeof null)       //object
+console.log(typeof undefined)  //undefined
+
+
