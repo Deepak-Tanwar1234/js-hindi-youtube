3b94ce69091c263078d78ff67eaf2dd68705dca0
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-16 13:07:40 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -34,6 +34,37 @@ let valueInString=String(scores)
 console.log(typeof valueInString)
 console.log(valueInString)
 
+//************operations ************
+
+//console.log(2+3)
+//console.log(2-3)
+//console.log(2*3)
+//console.log(2/3)
+//console.log(2%3)
+//console.log(2**2)  2 to power 2
+
+let str1="Deepak"
+let str2=" Tanwar"
+let str=str1+str2
+console.log(str)
+
+console.log("1"+2)    // read tc ecma script
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5%3)
+
+console.log(+true) // also try true+
+
+let num1,num2,num3   // not use this type of declaration
+    num1=num2=num3=2
+
+let gamecounter=100  // also read pre and post operator by mtn java script
+++gamecounter 
+console.log(gamecounter)    
+
+
+
 
 
 
