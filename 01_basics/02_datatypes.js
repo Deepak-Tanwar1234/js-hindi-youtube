5723447642890a208429de4abb63d0649c674401
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-16 10:51:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";  // treat all  js code as newer version
+
+//alert(2+2)  we are using js not browser
+
+
+console.log(2); console.log(4)
+console.log(3
+    +3
+)
+// code readability should be high
