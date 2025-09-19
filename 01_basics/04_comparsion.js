03cfb0b2f284dcb903ffaa3bb1362e7279ec9ec7
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-19 10:59:40 +0000

    comparsion of datatypes

--- 01_basics/04_comparsion.js
+++ 01_basics/04_comparsion.js
@@ -7,6 +7,10 @@
 console.log("2">1)
 console.log("02">1)
 
+/*prolemm arises when there is two differnt datatypes 
+ so avoid immplict conversion
+ */
+
 console.log(null > 0)
 console.log(null == 0)
 console.log(null >= 0)
