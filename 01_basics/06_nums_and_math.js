d8e80d9c40d7a4f9654211fde7492e3824256ff6
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-30 08:24:53 +0000

    nums_and_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -18,4 +18,25 @@ console.log(hundereds.toLocaleString('en-IN'))
 
 // ************ Maths *************
 
+console.log(Math)
 
+console.log(Math.abs(-4))
+console.log(Math.round(4.4)) //mostly use only round method
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,2,6,7))
+console.log(Math.max(4,2,6,7))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1) 
+
+const min=10
+const max=20
+
+console.log(Math.random())
+console.log(Math.random()*(max-min))
+console.log(Math.random()*(max-min)+1)
+console.log(Math.random()*(max-min)+1+min)
+console.log(Math.round(Math.random()*(max-min))+1+min)
