743db7e78c879862f47c1c34e35ee58625c038b9
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-30 06:28:16 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,12 +1,38 @@
 const name="Deepak Tanwar"
 const repoCount= 23
 
+
+console.log(typeof name)
 // console.log(name+ repoCount + "value")
 
 // always use string interpolation
 
 console.log(`Hello my name is ${name} and my repocount is
      ${repoCount}`)
+
+const gameName=new String("Deepak")  
+
+console.log(gameName[0])
+console.log(typeof gameName)
+console.log(gameName)
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt('1'))
+console.log(gameName.indexOf("p"))
+console.log(gameName.substring(0,4))
+console.log(gameName.slice(-3,5))
+console.log(gameName.split(1,4))
+
+const newString="  Deepak  "
+console.log(newString.trim())
+console.log(gameName.replace)
+
+const newString ="https://deepak.com/deepak%20tanwar"
+
+
+
 
 
 
