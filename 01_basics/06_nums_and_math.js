4a9391812b323507555e4b788d562fb60c986f5c
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-30 07:27:00 +0000

    nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+const score=400
+
+const balance=new Number(100)
+console.log(balance)
+
+console.log(typeof balance.toString())
+console.log( balance.toString().length)
+console.log( balance.toFixed(2))
+
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(5))
+
+const hundereds=1000000
+console.log(hundereds.toLocaleString('en-IN'))
+
+/* try min value in google inspect 
+ i.e (number object)number.MIN_VALUE */
+
+// ************ Maths *************
+
+
