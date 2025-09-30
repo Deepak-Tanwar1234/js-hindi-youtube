a237b2cf46ea789e39c8f13f31427735e7412a17
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-09-30 05:33:55 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,5 +1,13 @@
-let name="Deepak"
-let repoCount= 23
+const name="Deepak Tanwar"
+const repoCount= 23
 
-console.log(name+ repoCount + "value")
+// console.log(name+ repoCount + "value")
+
+// always use string interpolation
+
+console.log(`Hello my name is ${name} and my repocount is
+     ${repoCount}`)
+
+
+
 
