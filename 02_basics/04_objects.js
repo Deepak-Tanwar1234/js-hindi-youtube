9a4aab9dee6a37d92ee69d7a55c7b0e567dc68c6
Deepak Tanwar <tanwardeepak186@gmail.com>
2025-10-06 13:32:34 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,63 @@
+// its singleton objects
+const tinderuser1=new Object() 
+console.log(tinderuser1) 
+
+const tinderuser2={
+
+}  //Above object is a non-singleton object
+
+tinderuser2.id="123abc"
+tinderuser2.name="Deepak"
+tinderuser2.isLoggedIn=false
+
+console.log(tinderuser2)
+//const tinderuser3=Object.create()
+//console.log(tinderuser3)
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname:"Deepak",
+            lastname:"Tanwar"
+        }
+    }
+}
+console.log(regularUser)
+console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj4={5:"a",6:"b"}
+
+//const obj3={obj1,obj2}
+//console.log((obj3))
+//console.log(obj1)
+const obj3 = Object.assign({},obj1,obj2,obj4)
+console.log(obj3)
+console.log(obj1)
+
+const obj5={...obj1,...obj2}  
+console.log(obj5)
+
+//Array of objects
+const user=[
+    {
+        id:1,
+       email:"d@gmail.com"
+    },
+    {
+        id:2,
+       email:"p@gmail.com"
+    }
+]
+
+console.log(user[0].id)
+
+console.log(tinderuser2)
+console.log(Object.keys(tinderuser2))
+console.log(Object.values(tinderuser2))
+console.log(Object.entries(tinderuser2))
+console.log(tinderuser2.hasOwnProperty("location"))
+
+
